f318a7f473f9dbf3cd298274760b3ec03b9fd1b3
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-02 19:09:18 +0200

    Added DOM elements variables

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,10 @@
 let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
 let totalBudget = 0;
 
+const form = document.getElementById('form');
+const amountInput = document.getElementById('amount');
+const typeInput = document.getElementById('type');
+const dateInput = document.getElementById('date');
+const notesInput = document.getElementById('notes');
+const transactionsList = document.getElementById('transactions');
+const totalBudgetDisplay = document.getElementById('total-budget');
